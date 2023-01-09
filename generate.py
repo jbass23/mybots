e60d724512562fe4cbc09e47a9460c48af58bb18
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-09 15:16:51 -0600

    created body

--- generate.py
+++ generate.py
@@ -1,7 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
-
 x = 0
 y = 0
 z = 0.5
@@ -9,6 +7,18 @@ length = 1
 width = 1
 height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
 
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
+
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -8,7 +8,8 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
-for i in range(10000):
+for i in range(1000):
 	p.stepSimulation()
+	time.sleep(1/60)
 
 p.disconnect()
