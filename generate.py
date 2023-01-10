62c4ee5bef50ecdd18e1cc8c9b7c054ded208a90
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-09 19:27:04 -0600

    made body with torso and legs

--- generate.py
+++ generate.py
@@ -1,8 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-x = 0
-y = 0
-z = 0.5
 length = 1
 width = 1
 height = 1
@@ -10,13 +7,17 @@ height = 1
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[x-3, y+3, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[length, width, height])
     pyrosim.End()
 
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
     pyrosim.End()
 
 
