71a655bbbfe9b64eae2b4bdc9af7a3818c6f9dcd
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-15 17:27:17 -0600

    added numpy and saved sensor data

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 import time
 
 physicsClient = p.connect(p.GUI)
@@ -12,9 +13,12 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
-for i in range(1000):
+backLegSensorValues = numpy.zeros(100)
+
+for i in range(100):
 	p.stepSimulation()
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/60)
 
 p.disconnect()
+print(backLegSensorValues)
