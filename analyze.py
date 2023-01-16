b59773e62fd64913676cd69c466c213ae367cbb4
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-15 18:11:43 -0600

    put plots on same graph

--- analyze.py
+++ analyze.py
@@ -2,11 +2,7 @@ import numpy
 import matplotlib.pyplot
 
 backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
-print(backLegSensorValues)
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.show()
-
 frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
-print(frontLegSensorValues)
 matplotlib.pyplot.plot(frontLegSensorValues)
+matplotlib.pyplot.plot(backLegSensorValues)
 matplotlib.pyplot.show()
