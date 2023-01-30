34004a5a6f45a7b62006dd33dd84792c90311631
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-30 14:38:55 -0600

    evaluate

--- parallelhillclimber.py
+++ parallelhillclimber.py
@@ -16,30 +16,28 @@ class PARALLEL_HILL_CLIMBER:
             self.nextAvailableID += 1
 
     def Evolve(self):
-        for i in range(c.populationSize):
-            self.parents[i].Start_Simulation("DIRECT")
-
-        for i in range(c.populationSize):
-            self.parents[i].Wait_For_Simulation_To_End()
+        self.Evaluate(self.parents)
 
         for currentGeneration in range(c.numberOfGenerations):
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
-        # self.Spawn()
-        # self.Mutate()
-        # self.child.Evaluate("DIRECT")
+        self.Spawn()
+        self.Mutate()
+        self.Evaluate(self.children)
         # self.Print()
         # self.Select()
-        pass
 
     def Spawn(self):
-        self.child = copy.deepcopy(self.parent)
-        self.child.Set_ID(self.nextAvailableID)
-        self.nextAvailableID += 1
+        self.children = {}
+        for i in self.parents:
+            self.children[i] = copy.deepcopy(self.parents[i])
+            self.children[i].Set_ID(self.nextAvailableID)
+            self.nextAvailableID += 1
 
     def Mutate(self):
-        self.child.Mutate()
+        for i in self.children:
+            self.children[i].Mutate()
 
     def Select(self):
         if self.parent.fitness > self.child.fitness:
@@ -52,3 +50,10 @@ class PARALLEL_HILL_CLIMBER:
     def Show_Best(self):
         # self.parent.Evaluate("GUI")
         pass
+
+    def Evaluate(self, solutions):
+        for i in range(c.populationSize):
+            solutions[i].Start_Simulation("DIRECT")
+
+        for i in range(c.populationSize):
+            solutions[i].Wait_For_Simulation_To_End()
