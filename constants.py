80a3f973505282c725b8cd980b8870d0a15cc92e
jbass23 <jordanbass2024@u.northwestern.edu>
2023-01-30 00:22:36 -0600

    added evaluate functionality

--- constants.py
+++ constants.py
@@ -9,3 +9,7 @@ phaseOffsetBL = 0
 amplitudeFL = numpy.pi / 3
 frequencyFL = 4
 phaseOffsetFL = 0
+
+length = 1
+width = 1
+height = 1
--- generate.py
+++ generate.py
@@ -1,24 +1,21 @@
+import constants as c
 import pyrosim.pyrosim as pyrosim
 import random
-
-length = 1
-width = 1
-height = 1
 
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[-3, 3, 0.5], size=[c.length, c.width, c.height])
     pyrosim.End()
 
 
 def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[c.length, c.width, c.height])
     pyrosim.Send_Joint(name='Torso_BackLeg', parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[c.length, c.width, c.height])
     pyrosim.Send_Joint(name='Torso_FrontLeg', parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[c.length, c.width, c.height])
     pyrosim.End()
 
 def Generate_Brain():
