a0fe182005e6e44c54ef60d2a904e7474cc04c1d
jbass23 <jordanbass2024@u.northwestern.edu>
2023-02-20 22:57:48 -0600

    added brain capabilities

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 import constants as c
 import bodyplan
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 bp = bodyplan.BODY_PLAN()
 print("calling create blueprint")
@@ -27,36 +28,22 @@ def Create_Body():
 
 def Create_Brain():
     pyrosim.Start_NeuralNetwork(f"brain0.nndf")
+    sensorCount = 0
 
-    for i in range(len(links)):
-        pyrosim.Send_Sensor_Neuron(name=i, linkName=links[i].name)
+    for i in range(c.numSensorNeurons):
+        if bp.sensorBoolArray[i] == 1:
+            pyrosim.Send_Sensor_Neuron(name=sensorCount, linkName=f"Link{i}")
+            sensorCount += 1
 
     for i in range(len(joints)):
-        pyrosim.Send_Motor_Neuron(name=c.numSensorNeurons+i, jointName=joints[i].name)
+        pyrosim.Send_Motor_Neuron(name=sensorCount + i, jointName=joints[i].name)
 
-    for currentRow in range(c.numSensorNeurons):
+    weights = np.random.rand(sensorCount, c.numMotorNeurons) * 2 - 1
+
+    for currentRow in range(sensorCount):
         for currentColumn in range(c.numMotorNeurons):
-            pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn + c.numSensorNeurons,
-                                 weight=0)
-
-    # pyrosim.Send_Sensor_Neuron(name=0, linkName="Base")
-    # for i in range(c.numLinks):
-    #     pyrosim.Send_Sensor_Neuron(name=i + 1, linkName=f"Link{i}")
-    #
-    # pyrosim.Send_Motor_Neuron(name=c.numSensorNeurons, jointName="Base_Link0")
-    # for i in range(c.numLinks - 1):
-    #     pyrosim.Send_Motor_Neuron(name=c.numSensorNeurons + i + 1, jointName=f"Link{i}_Link{i + 1}")
-    #
-    # for currentRow in range(c.numSensorNeurons):
-    #     for currentColumn in range(c.numMotorNeurons):
-    #         # if currentRow <= 1:
-    #         #     pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn + c.numSensorNeurons,
-    #         #                          weight=self.weights[currentRow][(currentColumn + 1) // 2])
-    #         # else:
-    #         #     pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn + c.numSensorNeurons,
-    #         #                          weight=self.weights[(currentRow + 2) // 2][(currentColumn + 1) // 2])
-    #         pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn + c.numSensorNeurons,
-    #                              weight=1)
+            pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn + sensorCount,
+                                 weight=weights[currentRow][currentColumn])
 
     pyrosim.End()
 
