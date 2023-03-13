34634927fb561532bcd416626d8497be6a3833c5
jbass23 <jordanbass2024@u.northwestern.edu>
2023-03-13 00:06:56 -0500

    added legend

--- analyze.py
+++ analyze.py
@@ -23,4 +23,5 @@ for i in range(len(bestValues)):
 matplotlib.pyplot.title("Members With Best Fitness Across 5 Generations")
 matplotlib.pyplot.xlabel("Number of Generations")
 matplotlib.pyplot.ylabel("Fitness")
+matplotlib.pyplot.legend()
 matplotlib.pyplot.savefig("graphs/fitnessBests.png", format="png")
